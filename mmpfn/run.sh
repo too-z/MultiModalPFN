b73e98c4a39f1ed97b3f0e0b5c3f37a2bad9a3cf
too-z <tooz.service@gmail.com>
2025-11-03 22:03:12 +0900

    test run with logs

--- mmpfn/run.sh
+++ mmpfn/run.sh
@@ -1,7 +1,7 @@
-python run.py pad_ufes_20 >> pad_ufes_20.log
-python run.py cbis_ddsm mass >> cbis_ddsm_mass.log
-python run.py cbis_ddsm calc >> cbis_ddsm_calc.log
-python run.py petfinder all >> petfinder.log
-python run.py cloth >> cloth.log
-python run.py salary >> salary.log
-python run.py airbnb >> airbnb.log
+python run.py pad_ufes_20 >> logs/pad_ufes_20.log
+python run.py cbis_ddsm mass >> logs/cbis_ddsm_mass.log
+python run.py cbis_ddsm calc >> logs/cbis_ddsm_calc.log
+python run.py petfinder all >> logs/petfinder.log
+python run.py cloth >> logs/cloth.log
+python run.py salary >> logs/salary.log
+python run.py airbnb >> logs/airbnb.log
