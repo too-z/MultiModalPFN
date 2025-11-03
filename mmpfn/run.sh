9c748a8d28bece424ee4449bce60c7806eee0326
too-z <tooz.service@gmail.com>
2025-11-03 21:54:22 +0900

    run file and configs for all datasets

---
+++ mmpfn/run.sh
@@ -0,0 +1,7 @@
+python run.py pad_ufes_20 >> pad_ufes_20.log
+python run.py cbis_ddsm mass >> cbis_ddsm_mass.log
+python run.py cbis_ddsm calc >> cbis_ddsm_calc.log
+python run.py petfinder all >> petfinder.log
+python run.py cloth >> cloth.log
+python run.py salary >> salary.log
+python run.py airbnb >> airbnb.log
